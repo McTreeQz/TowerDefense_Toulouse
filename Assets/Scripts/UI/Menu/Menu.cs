e3dcca42e6440977b959234aa8f8e6e230168fb4
McTreeQz <93200861+McTreeQz@users.noreply.github.com>
2022-12-08 19:46:06 +0100

    update menu

--- Assets/Scripts/UI/Menu/Menu.cs
+++ Assets/Scripts/UI/Menu/Menu.cs
@@ -5,18 +5,43 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public GameObject battleMenu;
-
+    public GameObject mainMenu;
+    public GameObject title;
+    public GameObject book;
+    
     
 
     private void Start()
     {
         battleMenu.SetActive(false);
+        title.SetActive(true);
 
-        
+
+    }
+    private void Trigger(string trigger)
+    {
+        if (book != null)
+        {
+            var animator = book.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.SetTrigger(trigger);
+            }
+        }
+        if (mainMenu != null)
+        {
+            var animator = mainMenu.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.SetTrigger(trigger);
+            }
+        }
     }
     public void StartMenu()
     {
-        SceneManager.LoadScene(2);
+        
+        Trigger("Open");
+        title.SetActive(false);
 
     }
     public void BackMenu()
@@ -31,7 +56,7 @@ public class Menu : MonoBehaviour
     }
     public void battle721()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(2);
     }
 
     public void battle1218()
@@ -41,5 +66,10 @@ public class Menu : MonoBehaviour
     public void battle1814()
     {
         Debug.Log("1814");
+    }
+    private void Update()
+    {
+
+        
     }
 }
